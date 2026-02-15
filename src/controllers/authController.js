e3d5d2ded613b9ecb44c670e9d34500f03dd3915
SaGerz <genarosamuel24@gmail.com>
2026-02-15 16:46:01 +0700

    feat : add enpoint login api

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 const db = require('../config/db');
 const bcrypt = require('bcrypt');
+const generateToken = require('../utils/generateToken.js')
 
 exports.register = async (req, res) => {
     const connection = await db.getConnection();
@@ -75,4 +76,53 @@ exports.register = async (req, res) => {
     } finally {
     connection.release();
     }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // cek user
+    const [rows] = await db.query(
+      `SELECT id, name, email, password_hash, role, laundry_id
+       FROM users
+       WHERE email = ?`,
+      [email]
+    );
+
+    if (rows.length === 0) {
+      return res.status(401).json({
+        message: "Email not found"
+      });
+    }
+
+    const user = rows[0];
+
+    // cek password
+    const isMatch = await bcrypt.compare(password, user.password_hash);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Wrong password"
+      });
+    }
+
+    // generate token
+    const token = generateToken(user);
+
+    res.json({
+      message: "Login success",
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        role: user.role,
+        laundry_id: user.laundry_id
+      }
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
 };
