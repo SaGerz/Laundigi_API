670bbfd2bfe8c3c8e89c1cd48ebabb30fb11bea2
SaGerz <genarosamuel24@gmail.com>
2026-04-05 01:14:15 +0700

    reffactor : move auth login logic to services

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,7 @@
 const db = require('../config/db');
 const bcrypt = require('bcrypt');
 const generateToken = require('../utils/generateToken.js');
-const { registerService } = require('../services/authServices.js');
+const { registerService, loginServices } = require('../services/authServices.js');
 
 exports.register = async (req, res) => {
     const connection = await db.getConnection();
@@ -44,44 +44,16 @@ exports.register = async (req, res) => {
 
 exports.login = async (req, res) => {
   try {
-    const { email, password } = req.body;
-
-    // cek user
-    const [rows] = await db.query(
-      `SELECT id, name, email, password_hash, role, laundry_id
-       FROM users
-       WHERE email = ?`,
-      [email]
-    );
-
-    if (rows.length === 0) {
-      return res.status(401).json({
-        message: "Email not found"
-      });
-    }
-
-    const user = rows[0];
-
-    // cek password
-    const isMatch = await bcrypt.compare(password, user.password_hash);
-
-    if (!isMatch) {
-      return res.status(401).json({
-        message: "Wrong password"
-      });
-    }
-
-    // generate token
-    const token = generateToken(user);
+    const result = await loginServices(req.body, db)
 
     res.json({
       message: "Login success",
-      token,
+      token: result.token,
       user: {
-        id: user.id,
-        name: user.name,
-        role: user.role,
-        laundry_id: user.laundry_id
+        id: result.id,
+        name: result.name,
+        role: result.role,
+        laundry_id: result.laundry_id
       }
     });
 
